58271da87f57993c43a242aa91b2161456ce547e
1552980358 <1552980358@qq.com>
2023-03-11 04:12:09 +0800

    [Cosmos] Update methods

--- cosmos/src/main/java/projekt/cloud/piece/cosmos/Cosmos.kt
+++ cosmos/src/main/java/projekt/cloud/piece/cosmos/Cosmos.kt
@@ -1,7 +1,192 @@
 package projekt.cloud.piece.cosmos
 
+import android.os.Build
+import android.os.Bundle
+
 object Cosmos {
 
+    /**
+     * Consider a class called `Data`, which implements to interface [ByteArrayCosmos]
+     *
+     * In Kotlin
+     * ```Kotlin
+     *     import projekt.cloud.piece.cosmos.ByteArrayCosmos
+     *
+     *     class Data(): ByteArrayCosmos {
+     *         override fun saveData(): ByteArray {
+     *             val byteArray = ByteArray(getByteArraySize())
+     *             // Do something like
+     *             // copyDataIntoByteArray(byteArray)
+     *             return byteArray
+     *         }
+     *         override fun recoverData(byteArray: ByteArray) {
+     *             // Do something like
+     *             // recoverDataWithByteArray(byteArray)
+     *         }
+     *     }
+     * ```
+     *
+     * In Java
+     * ```Java
+     *     import projekt.cloud.piece.cosmos.ByteArrayCosmos;
+     *
+     *     public class Example implements ByteArrayCosmos {
+     *         @NotNull
+     *         @Override
+     *         public byte[] saveData() {
+     *             var byteArray = new byte[getByteArraySize()];
+     *             // Do something like
+     *             // copyDataIntoByteArray(byteArray)
+     *             return byteArray;
+     *         }
+     *         @Override
+     *         public void recoverData(@NotNull byte[] byteArray) {
+     *             // Do something like
+     *             // recoverDataWithByteArray(byteArray)
+     *         }
+     *     }
+     * ```
+     **/
 
+    /**
+     * Method [putCosmos]; Java method name `putCosmosToBundle()`:
+     * Put [ByteArrayCosmos] into [Bundle]
+     *
+     * @param name
+     * @param byteArrayCosmos
+     *
+     * Usage:
+     * In Kotlin
+     * ```Kotlin
+     *    import projekt.cloud.piece.cosmos.Cosmos.putCosmos
+     *    ...
+     *    val dat = Data()
+     *    getBundle().putCosmos("cosmos_name", dat)
+     * ```
+     * In Java
+     * ```Java
+     *     import projekt.cloud.piece.cosmos.Cosmos;
+     *     ...
+     *     var data = new Data();
+     *     Cosmos.putCosmosToBundle(getBundle(), "cosmos_name" data);
+     * ```
+     **/
+    @JvmName("putCosmosToBundle")
+    @JvmStatic
+    fun Bundle.putCosmos(name: String, byteArrayCosmos: ByteArrayCosmos) {
+        putSerializable(name, LibCosmos(byteArrayCosmos.saveData()))
+    }
+
+    /**
+     * Method [getCosmos]; Java method name `getCosmosFromBundle()`:
+     * Get ByteArray from Bundle, and provide stored [ByteArray] to [T],
+     * which implemented [ByteArrayCosmos].
+     * You should call [clearCosmos] when you do not need [LibCosmos.pointer] pointed [ByteArray] in
+     * memory anymore.
+     * Other than self calling [clearCosmos], using [extractCosmos] is advised to get a copy and
+     * release [LibCosmos.pointer] pointed memory space, letting JVM controls over the instance GC
+     *
+     * @param name
+     * @param block
+     *
+     * Usage:
+     * In Kotlin
+     * ```Kotlin
+     *     import projekt.cloud.piece.cosmos.Cosmos.getCosmos
+     *     ...
+     *     getBundle().getCosmos("cosmos_name") { Data() }
+     * ```
+     * In Java
+     * ```
+     *     import projekt.cloud.piece.cosmos.Cosmos;
+     *     ...
+     *     Cosmos.getCosmosFromBundle(getBundle(), "cosmos_name, Data::new);
+     * ```
+     **/
+    @JvmName("getCosmosFromBundle")
+    @JvmStatic
+    fun <T: ByteArrayCosmos> Bundle.getCosmos(name: String, block: () -> T): T {
+        return block.invoke().apply {
+            recoverData(getCosmos(name).byteArray)
+        }
+    }
+
+    /**
+     * Method [extractCosmos]; Java method name: `extractCosmosFromBundle()`;
+     * Same function as [getCosmos], but will clear cached [ByteArray] content.
+     * It is strongly advised to use this method to copy your own [ByteArray] instance,
+     * other than storing [ByteArray] pointed by [LibCosmos.pointer],
+     * which may lead to memory leaks if you forgot to release [LibCosmos.pointer] pointed [ByteArray]
+     * in memory by calling [clearCosmos].
+     * Getting a copy and release [LibCosmos.pointer] pointed memory space, and let JVM controls over
+     * the instance GC will gain a better memory performance.
+     *
+     * @param name
+     * @param block
+     *
+     * Usage:
+     * In Kotlin
+     * ```Kotlin
+     *     import projekt.cloud.piece.cosmos.Cosmos.getCosmos
+     *     ...
+     *     getBundle().getCosmos("cosmos_name") { Data() }
+     * ```
+     * In Java
+     * ```
+     *     import projekt.cloud.piece.cosmos.Cosmos;
+     *     ...
+     *     Cosmos.getCosmosFromBundle(getBundle(), "cosmos_name, Data::new);
+     * ```
+     **/
+    @JvmName("extractCosmosFromBundle")
+    fun <T: ByteArrayCosmos> Bundle.extractCosmos(name: String, block: () -> T): T {
+        return getCosmos(name).use { cosmos ->
+            block.invoke().apply {
+                recoverData(cosmos.byteArray)
+            }
+        }
+    }
+
+    /**
+     * Method [clearCosmos]; Java method name `clearCosmosFromBundle()`:
+     * Clear [LibCosmos.pointer] pointed memory, and release that
+     *
+     * Usage:
+     * In Kotlin
+     * ```Kotlin
+     *     import projekt.cloud.piece.cosmos.Cosmos.clearCosmos
+     *     ...
+     *     getBundle().clearCosmos("cosmos_name")
+     * ```
+     * In Java
+     * ```
+     *     import projekt.cloud.piece.cosmos.Cosmos;
+     *     ...
+     *     Cosmos.clearCosmosFromBundle(getBundle(), "cosmos_name");
+     * ```
+     **/
+    @JvmName("clearCosmosFromBundle")
+    fun Bundle.clearCosmos(name: String) {
+        getCosmosNullable(name)?.close()
+    }
+
+    private fun Bundle.getCosmosNullable(name: String): LibCosmos? {
+        if (name.isBlank() || !containsKey(name)) {
+            return null
+        }
+        return when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> {
+                getSerializable(name, LibCosmos::class.java)
+            }
+            else -> {
+                @Suppress("DEPRECATION")
+                getSerializable(name) as? LibCosmos
+            }
+        }
+    }
+
+    private fun Bundle.getCosmos(name: String): LibCosmos {
+        return getCosmosNullable(name)!!
+    }
 
 }
